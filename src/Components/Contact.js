e7364d7ccbf063d2cdab92d2a2111625b5c136b7
jennyjean <jennyjean8675309@yahoo.com>
2019-03-09 13:47:11 -0500

    Added boxes for contact info- Github and email, also added button for piano project

--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -3,10 +3,18 @@ import React, { Component } from 'react';
 class Contact extends Component {
   render() {
     return (
-      <div>
+      <div id='contact'>
         <p className='section-heading light'>where you can find me</p>
 
-        <div id='badge' class="LI-profile-badge" data-version="v1" data-size="large" data-locale="en_US" data-type="horizontal" data-theme="dark" data-vanity="jenny-j-ingram"><a class="LI-simple-link" href='https://www.linkedin.com/in/jenny-j-ingram?trk=profile-badge'>Jennifer Ingram</a></div>
+        <div id='contact-box'>
+          <div id='badge' className="LI-profile-badge" data-version="v1" data-size="large" data-locale="en_US" data-type="horizontal" data-theme="dark" data-vanity="jenny-j-ingram"><a className="LI-simple-link" href='https://www.linkedin.com/in/jenny-j-ingram?trk=profile-badge'>Jennifer Ingram</a></div>
+
+          <div id='contact-block'>
+          </div>
+
+          <div id='contact-block'>
+          </div>
+        </div>
       </div>
     )
   }
--- src/Components/Portfolio.js
+++ src/Components/Portfolio.js
@@ -30,6 +30,9 @@ class Portfolio extends Component {
           <div>
             <button id='rps' className='project-button' onClick={(e)=>this.handleOnClick(e)}>Rock, Paper, Scissors, Fight!</button>
           </div>
+          <div>
+            <button id='piano' className='project-button' onClick={(e)=>this.handleOnClick(e)}>Floor Piano</button>
+          </div>
         </div>
 
         <div id='musee-modal' className='modal'>
@@ -38,12 +41,10 @@ class Portfolio extends Component {
               <span className='close' onClick={(e)=>this.closeModal(e)}>Close</span>
               <h1>Musee</h1>
             </div>
-
             <div className='modal-body'>
               <p>A museum review website for both museum visitors and professionals alike</p>
               <iframe title='musee-demo' width="560" height="315" src="https://www.youtube.com/embed/i-6s3h1xDUc" frameBorder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
             </div>
-
             <div className='modal-footer'>
               <h3>Stack...</h3>
             </div>
@@ -56,12 +57,10 @@ class Portfolio extends Component {
               <span className='close' onClick={(e)=>this.closeModal(e)}>Close</span>
               <h1>Cabinets of Curiosity</h1>
             </div>
-
             <div className='modal-body'>
               <p>A place for users to share their unique perspective of the world through a digital collection</p>
               <iframe title='cabinets-demo' width="560" height="315" src="https://www.youtube.com/embed/7uV5yR_zG6c" frameBorder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
             </div>
-
             <div className='modal-footer'>
               <h3>Stack...</h3>
             </div>
@@ -74,19 +73,15 @@ class Portfolio extends Component {
               <span className='close' onClick={(e)=>this.closeModal(e)}>Close</span>
               <h1>Rock, Paper, Scissors, Fight!</h1>
             </div>
-
             <div className='modal-body'>
               <p>Rock, Paper, Scissors, Fight! - a card-based twist on the classic game</p>
               <iframe title='rps-demo' width="560" height="315" src="https://www.youtube.com/embed/uefyC_FiuBY" frameBorder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
             </div>
-
             <div className='modal-footer'>
               <h3>Stack...</h3>
             </div>
           </div>
         </div>
-
-
       </div>
     )
   }
