f2af17805622680cf9de75d156b07638a15cafc5
jennyjean <jennyjean8675309@yahoo.com>
2019-03-11 14:03:03 -0400

    Updates

---
+++ precache-manifest.1ead3e17825b09f867eec702713f0f19.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "82909797f29ad7bed193ec99e68e0e01",
+    "url": "/static/media/code_square.82909797.jpg"
+  },
+  {
+    "revision": "87c9babfa687286e0277",
+    "url": "/static/css/main.9f8fe52e.chunk.css"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.c5541365.js"
+  },
+  {
+    "revision": "ac164eeb7fe39897100f",
+    "url": "/static/js/2.aa7a0768.chunk.js"
+  },
+  {
+    "revision": "3d510aefb318ad16cf31895cf462f513",
+    "url": "/static/media/cropped_pic.3d510aef.png"
+  },
+  {
+    "revision": "87c9babfa687286e0277",
+    "url": "/static/js/main.b1d4336b.chunk.js"
+  },
+  {
+    "revision": "4c45bb7f3771967e0df03b107f2d5d32",
+    "url": "/static/media/rocket_square.4c45bb7f.jpg"
+  },
+  {
+    "revision": "e4114676be9c2c4a5e79e4018010f967",
+    "url": "/static/media/juju_square.e4114676.jpg"
+  },
+  {
+    "revision": "2cbdc273a1555fa512ff54595e819f32",
+    "url": "/static/media/J_Ingram_resume.2cbdc273.pdf"
+  },
+  {
+    "revision": "064ace4777ac86d7627f455f36975452",
+    "url": "/static/media/GitHub_Logo_White.064ace47.png"
+  },
+  {
+    "revision": "04b289cc4dfe244f5a36032b2ea4b5f2",
+    "url": "/index.html"
+  }
+];
